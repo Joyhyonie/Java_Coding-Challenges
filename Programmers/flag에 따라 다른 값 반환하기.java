94934b660e19385a7dd12e136b242e318c669219
Joyhyonie <cho970129@gmail.com>
2023-07-24 21:10:42 +0900

    🧩 Algorithm: 알고리즘 풀이

---
+++ Programmers/flag에 따라 다른 값 반환하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b, boolean flag) {
+        
+        return flag ? a + b : a - b;
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181933
