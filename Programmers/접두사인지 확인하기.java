ab1f766167d0f721c9af28773e26bfda80c1fd92
Joyhyonie <cho970129@gmail.com>
2023-08-06 22:36:42 +0900

    🧩 Algorithm: String.startsWith()를 활용한 접두사 확인

---
+++ Programmers/접두사인지 확인하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+            
+        return my_string.startsWith(is_prefix) ? 1 : 0;
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181906
