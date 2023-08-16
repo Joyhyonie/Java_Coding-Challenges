3ef9c8fadd77e44e81dece405482696c93106a6a
Joyhyonie <cho970129@gmail.com>
2023-08-16 22:12:15 +0900

    🧩 Algorithm: 알고리즘 풀이

---
+++ Programmers/문자열을 정수로 변환하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String n_str) {
+        
+        return Integer.parseInt(n_str);
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181848
