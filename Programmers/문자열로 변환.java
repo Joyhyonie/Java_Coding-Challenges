6df7620b6572939113307b3ccfeb2993cb277db4
Joyhyonie <cho970129@gmail.com>
2023-08-17 16:02:32 +0900

    🧩 Algorithm: String.valueOf() / Integer.toString() 활용

---
+++ Programmers/문자열로 변환.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(int n) {
+        return n + "";
+    }
+}
+
+/* String.valueOf() / Integer.toString() 활용 */
+class Solution {
+    public String solution(int n) {
+        return String.valueOf(n);
+    }
+}
+-------------------------------------------------
+class Solution {
+    public String solution(int n) {
+        return Integer.toString(n);
+    }
+}
+  
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181845
