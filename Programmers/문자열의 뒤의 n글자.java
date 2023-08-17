d0ed2ac61e97574461b4c3a23263dd65e8efdd91
Joyhyonie <cho970129@gmail.com>
2023-08-17 16:10:12 +0900

    🧩 Algorithm: substring(str - n) 활용

---
+++ Programmers/문자열의 뒤의 n글자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int n) {
+
+        return my_string.substring(my_string.length() - n);
+        // 뒤에서부터 n번째까지
+        
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181910
