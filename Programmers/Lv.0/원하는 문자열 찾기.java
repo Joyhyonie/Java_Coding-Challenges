61e5d0937b63f5238b7711c27ccf7ed8910ceef4
Joyhyonie <cho970129@gmail.com>
2023-08-20 18:40:29 +0900

    🧩 Algorithm: toUpperCase().contains() 활용

---
+++ Programmers/Lv.0/원하는 문자열 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        
+        return myString.toUpperCase().contains(pat.toUpperCase()) ? 1 : 0;
+      
+    }
+}
+
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181878
