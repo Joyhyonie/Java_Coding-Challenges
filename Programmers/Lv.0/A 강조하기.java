5b32033c5a938ee21481ca2e0b45057b54c05c3f
Joyhyonie <cho970129@gmail.com>
2023-08-24 21:30:12 +0900

    🧩 Algorithm: 알고리즘 풀이

---
+++ Programmers/Lv.0/A 강조하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        
+        myString = myString.toLowerCase();
+        
+        return myString.replace("a", "A");
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181874
